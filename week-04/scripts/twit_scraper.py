4bf44230ae0028d2677afebeeba345d8c3ff401b
Abob <abob@mit.edu>
2018-03-07 10:00:12 -0500

    adding twitter scraper stuff

---
+++ week-04/scripts/twit_scraper.py
@@ -0,0 +1,5 @@
+## Twitter scraper
+
+import jsonpickle
+import tweepy
+import pandas as pd
--- week-04/twitter_keys.py
+++ week-04/twitter_keys.py
@@ -1,2 +1,2 @@
-api_key = ""
-api_secret = ""
+api_key = "	PVNdR3ZyU6ER3VEIf3DVSaVXV"
+api_secret = "g2WxtyfTVXcMQFChxD93SA2a4Hfcqn3l6o2XnAyNUpZmpA2abG"
